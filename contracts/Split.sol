62edc36d4d160ac934115ee34e052c4ccf492971
0xvoidfarmer <smt.pr16@gmail.com>
2017-08-20 19:26:24 +0100

    Initial commit

---
+++ contracts/Split.sol
@@ -0,0 +1,100 @@
+pragma solidity ^0.4.5;
+
+contract Split{
+	
+	bool public isRunning;
+	address public owner;
+
+	struct claimStruct{
+		uint amountDue;
+		uint amountClaimed;
+	}
+
+	mapping (address => claimStruct) public claimStructs;
+
+	event LogDepositSplit(address sender, address receiver1, address receiver2, uint deposit, uint splitAmount);
+	event LogClaimedOwedSuccess(address claimant, uint amount);
+	event LogClaimedOwedFail(address claimant, uint amount);
+
+	function Split() {
+		owner = msg.sender;
+		isRunning = true;
+	}
+
+	function split(address _receiver1, address _receiver2 )
+		public 
+		payable
+		returns (bool success)
+	{
+		if(!isRunning) throw;
+		if(_receiver1 == address(0) || _receiver2 == address(0)) throw;
+		if(msg.value < 2) throw;
+
+		var splitAmount = msg.value / 2; 
+
+		if (2 * splitAmount > msg.value) {
+			claimStructs[msg.sender].amountDue += 1;
+		}
+
+		claimStructs[_receiver1].amountDue += splitAmount;
+		claimStructs[_receiver2].amountDue += splitAmount;
+
+		LogDepositSplit(msg.sender, _receiver1,_receiver2, msg.value, splitAmount);
+
+		return true;
+	}
+
+	function fetchOwedAmount(address claimant)
+		public
+		constant
+		returns(uint dueAmount)
+	{
+		if(!isRunning) throw;
+		return claimStructs[claimant].amountDue;
+	}
+
+	function fetchClaimedAmount(address claimant)
+		public
+		constant
+		returns(uint claimedAmount)
+	{
+		if(!isRunning) throw;
+		return claimStructs[claimant].amountClaimed;
+	}
+
+	function claim()
+		public 
+		returns (bool success)
+	{	
+		if(!isRunning) throw;
+		if(claimStructs[msg.sender].amountDue == 0) throw;
+
+		var amountToClaim = claimStructs[msg.sender].amountDue;
+		if(amountToClaim == 0) throw;
+		
+		//Optimistic accounting
+		claimStructs[msg.sender].amountDue -= amountToClaim;
+	    claimStructs[msg.sender].amountClaimed += amountToClaim;
+	    
+		if(!msg.sender.send(amountToClaim)) throw;
+	
+		LogClaimedOwedSuccess(msg.sender,amountToClaim);
+
+		return true;
+	}
+
+	function switchRunning (bool onOff)
+		public 
+		returns( bool sucess){
+		if(msg.sender != owner) throw;
+	    isRunning = onOff;
+	}
+
+	function killContract (){
+		if(msg.sender != owner) throw;
+	    suicide(owner);
+	}
+
+	function (){}
+
+}
---
+++ test/split.js
@@ -0,0 +1,132 @@
+var Split = artifacts.require("./Split.sol");
+
+contract("Splitter", function(accounts) {
+	var splitContract;
+	var owner = accounts[0];
+	var sender = accounts[1];
+	var receiver1 = accounts[2];
+	var receiver2 = accounts[3];
+	var notInvited = accounts[4];
+
+	beforeEach(function(){
+		return Split.new({from: owner})
+		.then(function(instance){
+			splitContract = instance;
+		});
+	});
+
+	//constructor 
+	it("should be owned by owner", function(){
+		return splitContract.owner()
+		.then(function(_actualOwner){
+			assert.strictEqual(_actualOwner, owner,"contract is not owned by owner"); 		
+		});
+	});
+
+	it("should have a running state of true on construction", function(){
+		return splitContract.isRunning()
+		.then(function(_actualState){
+			assert.strictEqual(_actualState, true,"contract initial state is not set to true"); 		
+		});
+	});
+
+
+	//split 
+
+	it("should split sender's deposit equally between receiver 1 and 2 ", function(){
+		var depositToSplit = 42;	
+		
+		return splitContract.split(receiver1,receiver2, {from:sender, value: depositToSplit })
+		.then(function(txn){
+			return splitContract.fetchOwedAmount(receiver1);
+		})
+		.then(function(_receiver1Amount) {	
+			assert.equal(_receiver1Amount,21 , "receiver1's owed amount is not correct");
+			return splitContract.fetchOwedAmount(receiver2);
+		}).then(function(_receiver2Amount) {	
+			assert.equal(_receiver2Amount, 21 , "receiver2's owed amount is not correct");
+			return splitContract.fetchOwedAmount(notInvited);
+		}).then(function(_notInvitedOwed) {	
+			assert.equal(_notInvitedOwed, 0, "NotInvited's owed amount is not correct");
+		});		
+		
+	});
+
+	it("should split sender's odd number deposit and add remainder to sender ", function(){
+		var depositToSplit = 43;	
+		
+		return splitContract.split(receiver1,receiver2, {from:sender, value: depositToSplit })
+		.then(function(txn){
+			return splitContract.fetchOwedAmount(receiver1);
+		})
+		.then(function(_receiver1Amount) {	
+			assert.equal(_receiver1Amount,21 , "receiver1's owed amount is not correct");
+			return splitContract.fetchOwedAmount(receiver2);
+		}).then(function(_receiver2Amount) {	
+			assert.equal(_receiver2Amount, 21 , "receiver2's owed amount is not correct");
+			return splitContract.fetchOwedAmount(sender);
+		}).then(function(_senderRemainder) {	
+			assert.equal(_senderRemainder, 0, "Sender's remiander amount is not correct");
+		});				
+	});
+
+	//claim
+	it("should allow receiver to claim what is due", function(){
+		var depositToSplit = 42;
+		return splitContract.split(receiver1,receiver2, {from:sender, value: depositToSplit })
+		.then(function(txn){
+			return splitContract.claim({from: receiver1})
+			.then(function(){
+				return splitContract.fetchClaimedAmount(receiver1);
+			}).then(function(_claimedAmount) {	
+				assert.equal(_claimedAmount, 21 , "receiver1's claimed amount is not correct");
+				return splitContract.fetchOwedAmount(receiver1);
+			}).then(function(_owedAmount){
+				assert.equal(_owedAmount, 0 , "receiver1's owed amount is not correct");
+			});
+		});
+	});
+
+	//pause
+	it("should not allow non-owner to switch running state", function(){
+			return splitContract.switchRunning(false, {from: notInvited})
+			.catch(function(err){
+				assert(err.message.length > 0, 'non-owner is not  allowed to execute this function');
+			})
+	});
+
+	it("should render all functions unusable", function(){
+		return splitContract.switchRunning(false, {from:owner})
+		.then(function(txn){
+			return splitContract.split(receiver1,receiver2, {from:sender, value: 42 })
+			//.then(function(_splitTxn){})
+			.catch(function(err){
+			 	assert(err.message.length > 0, 'function execution during contract not runing state is not correct');
+			});
+		});
+   		
+	});
+
+	//kill
+	it("should not allow non-owner to kill contract",function(){
+		return splitContract.killContract({from: notInvited})
+		.catch(function(err){
+			assert(err.message.length > 0, 'non-owner is not  allowed to kill contract');			
+		});
+	});
+
+	it("should kill contract",function(){
+		return splitContract.killContract({from: owner})
+		.then( function(txn){
+			return splitContract.split(receiver1,receiver2, {from:sender, value: 42 })			
+			.catch(function(err){
+			 	assert(err.message.length > 0, 'contract is suppoised to be dead / sucided');
+			});
+		});
+	});
+
+
+	
+
+
+}); //</contract>
